538273ce33205f3eb043c288bad9a046922177ea
prasanth <prasanthb313@gmail.com>
2026-02-17 20:41:07 +0530

    17th Feb

---
+++ TypeScript/sampletest.ts
@@ -0,0 +1,17 @@
+
+console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");
+
+console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");
+
+console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");
+
+console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");
+
